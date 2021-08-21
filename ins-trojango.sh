8246d7efc2241e5ed21bb5d5b1b6b52de90aab64
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-21 17:43:06 +0700

    Update ins-trojango.sh

--- ins-trojango.sh
+++ ins-trojango.sh
@@ -51,7 +51,7 @@ cat > /etc/trojan-go/config.json << END
 {
   "run_type": "server",
   "local_addr": "0.0.0.0",
-  "local_port": 2096,
+  "local_port": 443,
   "remote_addr": "127.0.0.1",
   "remote_port": 81,
   "log_level": 1,
@@ -152,8 +152,8 @@ cat > /etc/trojan-go/config.json << END
 }
 
 END
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2096 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p udp --dport 2096 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p udp --dport 443 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 iptables-restore -t < /etc/iptables.up.rules
 netfilter-persistent save
