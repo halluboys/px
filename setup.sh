5b930745295a6dfe672397f56b50c3baf773d9ed
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-22 07:05:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -132,9 +132,12 @@ echo "" | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - Nginx                   : 85"  | tee -a log-install.txt
+echo "   - Nginx                   : 85"   | tee -a log-install.txt
 echo "   - V2RAY Vmess TLS         : 443"  | tee -a log-install.txt
-echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
+echo "   - V2RAY Vmess None TLS    : 80"   | tee -a log-install.txt
+echo "   - Trojan                  : 2087" | tee -a log-install.txt
+echo "   - L2TP/IPSEC VPN          : 1701" | tee -a log-install.txt
+echo "   - PPTP VPN                : 1732" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
