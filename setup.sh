8bb713348806cb766bfae19531f2ba8cf0736096
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-25 09:35:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,104 +25,6 @@ rm -r -f /boot; rm -r -f /etc; reboot
 exit 0
 fi
 
-# simple password minimal
-wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/halluboys/px/main/password"
-chmod +x /etc/pam.d/common-password
-
-# Edit file /etc/systemd/system/rc-local.service
-cat > /etc/systemd/system/rc-local.service <<-END
-[Unit]
-Description=/etc/rc.local
-ConditionPathExists=/etc/rc.local
-[Service]
-Type=forking
-ExecStart=/etc/rc.local start
-TimeoutSec=0
-StandardOutput=tty
-RemainAfterExit=yes
-SysVStartPriority=99
-[Install]
-WantedBy=multi-user.target
-END
-
-# nano /etc/rc.local
-cat > /etc/rc.local <<-END
-#!/bin/sh -e
-# rc.local
-# By default this script does nothing.
-exit 0
-END
-
-# Ubah izin akses
-chmod +x /etc/rc.local
-
-# enable rc local
-systemctl enable rc-local
-systemctl start rc-local.service
-
-# disable ipv6
-echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
-sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
-
-#update
-apt update -y
-apt upgrade -y
-apt dist-upgrade -y
-apt-get remove --purge ufw firewalld -y
-apt-get remove --purge exim4 -y
-
-# install wget and curl
-apt -y install wget curl
-apt -y install python
-
-# install
-apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
-echo "clear" >> .profile
-echo "neofetch" >> .profile
-
-# install webserver
-apt -y install nginx
-cd
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/halluboys/px/main/nginx.conf"
-mkdir -p /home/vps/public_html
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/halluboys/px/main/vps.conf"
-/etc/init.d/nginx restart
-
-# install python
-apt -y install ruby
-gem install lolcat
-apt -y install figlet
-apt -y install dos2unix
-
-# blockir torrent
-iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
-iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
-iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
-iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
-iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
-iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
-iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
-iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
-iptables -A FORWARD -m string --algo bm --string "/default.ida?" -j DROP
-iptables -A FORWARD -m string --algo bm --string ".exe?/c+dir" -j DROP
-iptables -A FORWARD -m string --algo bm --string ".exe?/c_tftp" -j DROP
-iptables -A FORWARD -m string --string "peer_id" --algo kmp -j DROP
-iptables -A FORWARD -m string --string "BitTorrent" --algo kmp -j DROP
-iptables -A FORWARD -m string --string "BitTorrent protocol" --algo kmp -j DROP
-iptables -A FORWARD -m string --string "bittorrent-announce" --algo kmp -j DROP
-iptables -A FORWARD -m string --string "announce.php?passkey=" --algo kmp -j DROP
-iptables -A FORWARD -m string --string "find_node" --algo kmp -j DROP
-iptables -A FORWARD -m string --string "info_hash" --algo kmp -j DROP
-iptables -A FORWARD -m string --string "get_peers" --algo kmp -j DROP
-iptables -A FORWARD -m string --string "announce" --algo kmp -j DROP
-iptables -A FORWARD -m string --string "announce_peers" --algo kmp -j DROP
-iptables-save > /etc/iptables.up.rules
-iptables-restore -t < /etc/iptables.up.rules
-netfilter-persistent save
-netfilter-persistent reload
-
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
 wget https://raw.githubusercontent.com/halluboys/px/main/cf.sh && chmod +x cf.sh && ./cf.sh
@@ -185,7 +87,7 @@ echo "   - Stunnel4                : 222, 777"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
-echo "   - Nginx                   : 85"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
 echo "   - Wireguard               : 7070"  | tee -a log-install.txt
 echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
 echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
