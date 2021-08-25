8baf7516eb52ed11aa39982eae49e87cb777c73b
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-25 17:37:20 +0700

    Update ins-vt.sh

--- v2rayy/ins-vt.sh
+++ v2rayy/ins-vt.sh
@@ -22,7 +22,7 @@ bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quicksta
 mkdir /root/.acme.sh
 curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
 chmod +x /root/.acme.sh/acme.sh
-sudo /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 service squid start
 uuid=$(cat /proc/sys/kernel/random/uuid)
