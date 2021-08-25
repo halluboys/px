2f3b32a3aeecf0c211f9ce22bfbc882df59da640
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-25 10:16:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,9 +33,9 @@ wget https://raw.githubusercontent.com/halluboys/xzvnct/main/sstp.sh && chmod +x
 #install ssr
 wget https://raw.githubusercontent.com/halluboys/xzvnct/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
 wget https://raw.githubusercontent.com/halluboys/xzvnct/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
-#install wg
 #install v2ray
-wget https://raw.githubusercontent.com/halluboys/px/main/v2rayy/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/lesta-1/wisang/main/vray/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+#install wg
 wget https://raw.githubusercontent.com/halluboys/xzvnct/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install L2TP
 wget https://raw.githubusercontent.com/halluboys/xzvnct/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
