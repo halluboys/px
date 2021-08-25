bd3099097aa863b52f381829cf506aabaeb25ec4
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-25 14:09:40 +0700

    Update cert.sh

--- v2rayy/cert.sh
+++ v2rayy/cert.sh
@@ -2,9 +2,8 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
+IP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-clear
 echo start
 sleep 0.5
 source /var/lib/premium-script/ipvps.conf
