2a7e423552f522e78197c16627f3874c18e6d467
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-30 02:43:27 +0700

    Update ins-vt.sh

--- v2rayy/ins-vt.sh
+++ v2rayy/ins-vt.sh
@@ -529,7 +529,7 @@ chmod +x cf
 chmod +x certv2ray
 cd
 rm -rf ins-vt.sh
-mv /root/domain /etc/v2ray
+rsync -a /root/domain /etc/v2ray
 echo "0 0 * * * root xp-ws" >> /etc/crontab
 echo "0 0 * * * root xp-tr" >> /etc/crontab
 echo "0 0 * * * root xp-vless" >> /etc/crontab
