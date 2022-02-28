26791122b683289c2841c1546bfd34140eb474ef
halluboys <87511503+halluboys@users.noreply.github.com>
2022-02-28 22:21:51 +0700

    Update certyy.sh

--- certyy.sh
+++ certyy.sh
@@ -54,15 +54,11 @@ echo $SUB_DOMAIN > /root/domain
 systemctl stop v2ray
 systemctl stop v2ray@none
 #add certificate
-sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs
-kill
 cd /root/
 mkdir /root/.acme.sh
 curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
-chmod +x /root/.acme.sh/acme.sh 
-rm acme.sh
+chmod +x /root/.acme.sh/acme.sh  
 cd .acme.sh
-echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
 sudo bash acme.sh --register-account -m hammysayang@gmail.com
 sudo bash acme.sh --issue --standalone -d $SUB_DOMAIN  --force
 sudo bash acme.sh --installcert -d $SUB_DOMAIN --fullchainpath
