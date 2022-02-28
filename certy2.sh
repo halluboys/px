c4b0bc6bbf6bf9a4365cc460cf8599cb2a1eb272
halluboys <87511503+halluboys@users.noreply.github.com>
2022-02-28 22:07:18 +0700

    Update certy2.sh

--- certy2.sh
+++ certy2.sh
@@ -60,7 +60,7 @@ chmod +x /root/.acme.sh/acme.sh
 rm acme.sh
 cd .acme.sh
 echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
- sudo bash acme.sh --register-account -m hammysayang@gmail.com
+sudo bash acme.sh --register-account -m hammysayang@gmail.com
 sudo bash acme.sh --issue --standalone -d $SUB_DOMAIN  --force
 sudo bash acme.sh --installcert -d $SUB_DOMAIN --fullchainpath
 sude bash acme.sh --installcert -d $SUB_DOMAIN --fullchainpath /etc/v2ray/v2ray.crt --ke$echo Done
